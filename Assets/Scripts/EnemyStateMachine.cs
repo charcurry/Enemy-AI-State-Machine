74cb71e5bbdbb0cc094fbbcdeeb86282647c2d55
Charlie <charlieryancurry@gmail.com>
2024-03-08 14:12:45 -0400

    implement most functionality of all states

--- Assets/Scripts/EnemyStateMachine.cs
+++ Assets/Scripts/EnemyStateMachine.cs
@@ -8,27 +8,51 @@ using UnityEngine.AI;
 
 public class EnemyStateMachine : MonoBehaviour
 {
+    // General variables used in most if not all States.
     private NavMeshAgent navMeshAgent;
     public Vector3 target;
     private FPS_Controller player;
 
-    public Material patrol;
-    public Material chase;
-    public Material search;
-    public Material attack;
-    public Material retreat;
-
+    // The rule of thumb im using for the variables is that for the most part, they are self contained, but
+    // variables such as chaseDistance will be used in other States as if the enemy is in the retreat state
+    // but the player gets close, the enemy will need to start chasing the player. This is how I have all of
+    // the variables & methods laid out.
     #region Patrol Variables
 
     [SerializeField] private GameObject[] waypoints;
-
     public int waypointIndex = 0;
+    public Material patrol;
 
     #endregion
 
     #region Chase Variables
 
     public int chaseDistance;
+    public Material chase;
+
+    #endregion
+
+    #region Search Variables
+
+    public Vector3 lastPlayerPosition;
+    public float searchTimerDuration;
+    [SerializeField] private float searchTimer;
+    public Material search;
+
+    #endregion
+
+    #region Attack Variables
+
+    public int attackDistance;
+    public float attackTimerDuration;
+    [SerializeField] private float attackTimer;
+    public Material attack;
+
+    #endregion
+
+    #region Retreat Variables
+
+    public Material retreat;
 
     #endregion
 
@@ -50,13 +74,13 @@ public class EnemyStateMachine : MonoBehaviour
         InitWaypoints();
         navMeshAgent = GetComponent<NavMeshAgent>();
         enemyState = EnemyStates.patrol;
+        searchTimer = searchTimerDuration;
+        attackTimer = attackTimerDuration;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //Debug.Log(enemyState);
-
         switch (enemyState)
         {
             case EnemyStates.patrol:
@@ -65,7 +89,6 @@ public class EnemyStateMachine : MonoBehaviour
                 navMeshAgent.SetDestination(target);
                 if (Vector3.Distance(transform.position, target) < 1)
                 {
-                    //Debug.Log("Reached Waypoint!");
                     UpdateWaypoint();
                 }
 
@@ -73,31 +96,75 @@ public class EnemyStateMachine : MonoBehaviour
                 {
                     enemyState = EnemyStates.chase;
                 }
-
-                    break; 
+                break;
+                
             case EnemyStates.chase:
                 GetComponent<Renderer>().material = chase;
                 target = player.transform.position;
                 navMeshAgent.SetDestination(target);
+                lastPlayerPosition = player.transform.position;
+                if (Vector3.Distance(transform.position, target) > chaseDistance)
+                {
+                    enemyState = EnemyStates.search;
+                }
+                if (Vector3.Distance(transform.position, target) < attackDistance)
+                {
+                    enemyState= EnemyStates.attack;
+                }
                 break;
+
             case EnemyStates.search:
                 GetComponent<Renderer>().material = search;
-                //
-                //
+                target = lastPlayerPosition;
+                navMeshAgent.SetDestination(target);
+                searchTimer -= Time.deltaTime;
+                if (Vector3.Distance(transform.position, player.transform.position) < chaseDistance)
+                {
+                    enemyState = EnemyStates.chase;
+                    searchTimer = searchTimerDuration;
+                }
+                if (searchTimer <= 0f)
+                {
+                    enemyState = EnemyStates.retreat;
+                    searchTimer = searchTimerDuration;
+                }
                 break;
+
             case EnemyStates.attack:
                 GetComponent<Renderer>().material = attack;
-                //
-                //
+                attackTimer -= Time.deltaTime;
+                if (attackTimer <= 0f)
+                {
+                    if (Vector3.Distance(transform.position, player.transform.position) > attackDistance)
+                    {
+                        enemyState = EnemyStates.chase;
+                        attackTimer = attackTimerDuration;
+                    }
+                    else
+                    {
+                        attackTimer = attackTimerDuration;
+                    }
+                }
                 break;
+
             case EnemyStates.retreat:
                 GetComponent<Renderer>().material = retreat;
-                //
-                //
+                target = GetClosestWaypoint().transform.position;
+                navMeshAgent.SetDestination(target);
+                if (Vector3.Distance(transform.position, target) < 1)
+                {
+                    enemyState = EnemyStates.patrol;
+                }
+                if (Vector3.Distance(transform.position, player.transform.position) < chaseDistance)
+                {
+                    enemyState = EnemyStates.chase;
+                }
                 break;
         }
     }
 
+    // These are the only two States that really needed external methods to make everything look cleaner.
+    // They are only used in the specified State. 
     #region Patrol Methods
 
     public void UpdateWaypoint()
@@ -112,6 +179,32 @@ public class EnemyStateMachine : MonoBehaviour
     public void InitWaypoints()
     {
         waypoints = GameObject.FindGameObjectsWithTag("Waypoint");
+    }
+
+    #endregion
+
+    #region Retreat Methods
+
+    public GameObject GetClosestWaypoint()
+    {
+        if (waypoints.Length != 0)
+        {
+            GameObject closestWaypoint = waypoints[0];
+            float closestDistance = Vector3.Distance(transform.position, closestWaypoint.transform.position);
+
+            for (int i = 1; i < waypoints.Length; i++)
+            {
+                float distance = Vector3.Distance(transform.position, waypoints[i].transform.position);
+                if (distance < closestDistance)
+                {
+                    closestWaypoint = waypoints[i];
+                    closestDistance = distance;
+                }
+            }
+
+            return closestWaypoint;
+        }
+        return null;
     }
 
     #endregion
